20e8ae93ac64311a744c63868749ee2a44d0cf72
Marcin Woźniak <ciapekm@gmail.com>
2017-03-24 11:58:50 +0100

    Changed color scheme for left menu.

--- frontend/themes/agency/layouts/homepage.php
+++ frontend/themes/agency/layouts/homepage.php
@@ -1,8 +1,7 @@
 <?php
 use yii\web\View;
 
-
-
+$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@dixonsatit/agencyTheme/dist');
 $this->registerJsFile($directoryAsset.'/js/cbpAnimatedHeader.min.js',['position'=>View::POS_END]);
 ?>
 <?php $this->beginContent('@dixonsatit/agencyTheme/views/layouts/_base.php'); ?>
